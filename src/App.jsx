995e11eb1f03c409b3fedb5d0064d395257bb031
Micah Tinife <micahking2k@gmail.com>
2023-07-27 22:27:09 -0400

    worked on the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './style.css'
+import logoLight from '../public/assets/logo-white.svg'
+import 'remixicon/fonts/remixicon.css'
+import '../node_modules/@vscode/codicons/dist/codicon.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  return <>
+    <section className="vsc__themer">
+      <aside className="vsc__themer__menu">
+        <div className="vsc__themer__menu__logo aside-container">
+            <img src={logoLight} alt="Dynamic VS Code Themer Logo Light" />
+            <span>VS Code Themer</span>
+        </div>
+        <div className="vsc__themer__name aside-container">
+          <input type="text" placeholder="Theme Name" />
+        </div>
+        <div className="vsc__themer__type aside-container">
+          <span>theme type</span>
+          <div className="vsc__themer__type__btn">
+            <span>dark</span>
+            <span>light</span>
+          </div>
+        </div>
+        <div className="vsc__themer__sections">
+          <span className="aside-container">sections</span>
+          <ul>
+            <li className="aside-container"><i class="ri-layout-top-line"></i><span>title bar</span></li>
+          </ul>
+        </div>
+      </aside>
+      <section className="vsc__themer__preview">
+        
+      </section>
+    </section>
+  </>
 }
 
-export default App
