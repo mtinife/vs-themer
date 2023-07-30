050d60f003d2679ff22d2a6bb789e1851a948718
Micah Tinife <micahking2k@gmail.com>
2023-07-30 00:26:20 -0400

    layouted aside panel

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,25 @@ export default function App() {
           <input type="text" placeholder="Theme Name" />
         </div>
         <div className="vsc__themer__type aside-container">
-          <span>theme type</span>
+          <span className="aside-header">theme type</span>
           <div className="vsc__themer__type__btn">
-            <span>dark</span>
+            <span className="modeActive">dark</span>
             <span>light</span>
           </div>
         </div>
         <div className="vsc__themer__sections">
-          <span className="aside-container">sections</span>
+          <span className="aside-header aside-container">sections</span>
           <ul>
-            <li className="aside-container"><i class="ri-layout-top-line"></i><span>title bar</span></li>
+            <li><i class="ri-layout-top-line"></i><span>title bar</span></li>
+            <li><i class="ri-layout-left-line"></i><span>activity bar</span></li>
+            <li><i class="ri-layout-right-2-line"></i><span>scrollbar control</span></li>
+            <li><i class="ri-edit-box-line"></i><span>editor</span></li>
+            <li><i class="ri-layout-left-2-line"></i><span>side bar</span></li>
+            <li><i class="ri-layout-bottom-line"></i><span>status bar</span></li>
+            <li><i class="ri-terminal-window-fill"></i><span>terminal</span></li>
           </ul>
         </div>
+        <button className="aside-container asideBtnStyle">Get Theme JSON</button>
       </aside>
       <section className="vsc__themer__preview">
         
